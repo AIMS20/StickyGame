566515e443767a2e130fd85dfd4a0a4a70384aa8
AIMS20 <58343132+AIMS20@users.noreply.github.com>
2021-04-28 15:32:12 +0200

    implemented basic menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject[] gamestateObjects;
+    private GameObject[] menustateObjects;
+    private GameObject uiManager;
+    private Transform canvas;
+
+    private void Awake()
+    {
+        uiManager = gameObject;
+        gamestateObjects = GameObject.FindGameObjectsWithTag("GamestateObjects");
+        menustateObjects = GameObject.FindGameObjectsWithTag("MenustateObjects");
+        canvas = uiManager.transform.Find("Canvas");
+        var playButton = GetButton("Play");
+        playButton.onClick.AddListener(PlayClick); //TODO: somehow integrate into GetButton
+        
+        var optionsButton = GetButton("Options");
+        optionsButton.onClick.AddListener(OptionsClick);
+        
+        var quitButton = GetButton("Quit");
+        quitButton.onClick.AddListener(QuitClick); //TODO: somehow integrate into GetButton
+    }
+
+    private void QuitClick()
+    {
+        print("clicked quit");
+        Application.Quit();
+    }
+
+    private Button GetButton(string name)
+    {
+        var button = canvas.Find(name).GetComponent<Button>();
+
+        return button;
+    }
+    
+    private void OptionsClick()
+    {
+        print("clicked options");
+        SetState("Options");
+    }
+
+
+    private void PlayClick()
+    {
+        print("clicked play");
+        SetState("Game");
+    }
+
+
+    void Start()
+    {
+        SetState("Menu");
+    }
+
+    private void SetState(string state)
+    {
+        switch (state)
+        {
+            case "Menu":
+                
+                Time.timeScale = 0;
+                foreach (var obj in gamestateObjects){
+                    obj.SetActive(false);
+                }
+                foreach (var obj in menustateObjects){
+                    obj.SetActive(true);
+                }
+                break;
+            
+            case "Game":
+                Time.timeScale = 1;
+                foreach (var obj in gamestateObjects){
+                    obj.SetActive(true);
+                }
+                foreach (var obj in menustateObjects){
+                    obj.SetActive(false);
+                }
+                break; 
+            
+            case "Options":
+                //TODO:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
