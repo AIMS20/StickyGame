40cacbdd7696eb4edf556f0a29addcf2183dd262
AIMS20 <58343132+AIMS20@users.noreply.github.com>
2021-06-14 19:34:20 +0200

    refactored to use Unity OnClick() in Editor

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 
@@ -10,32 +11,42 @@ public class UIManager : MonoBehaviour
 {
     private GameObject[] gamestateObjects;
     private GameObject[] menustateObjects;
+    private GameObject[] optionstateObjects;
     private GameObject uiManager;
     private Transform canvas;
-    private enum States{
-        MENU, GAME
+    public enum States{
+        MENU, OPTIONS, GAME, QUIT
     }
 
-    private States currentState;
+    public States currentState;
     
     private void Awake()
     {
+        SetState(0);
+        
         uiManager = gameObject;
+        
+        
         gamestateObjects = GameObject.FindGameObjectsWithTag("GamestateObjects");
         menustateObjects = GameObject.FindGameObjectsWithTag("MenustateObjects");
-        canvas = uiManager.transform.Find("Canvas");
+        // optionstateObjects = GameObject.FindGameObjectsWithTag("OptionsObjects");
+        
+        canvas = uiManager.transform.Find("MainMenu");
+        
+        
         var playButton = GetButton("Play");
-        playButton.onClick.AddListener(PlayClick); //TODO:  integrate into GetButton
+        // playButton.onClick.AddListener(PlayClick); //TODO:  integrate into GetButton
 
         var quitButton = GetButton("Quit");
-        quitButton.onClick.AddListener(QuitClick); //TODO:  integrate into GetButton
+        // quitButton.onClick.AddListener(QuitClick); //TODO:  integrate into GetButton
+        
+        EventSystem.current.SetSelectedGameObject(playButton.gameObject);
     }
 
-    private void QuitClick()
+    private void Start()
     {
-        print("clicked quit");
-        Application.Quit();
     }
+    
 
     private Button GetButton(string name)
     {
@@ -44,49 +55,40 @@ public class UIManager : MonoBehaviour
         return button;
     }
 
-
-    private void PlayClick()
+    public void Test(States state)
     {
-        // print("clicked play");
-        SetState(States.GAME);
+        
     }
-
-
-    void Start()
-    {
-        SetState(States.MENU);
-    }
-
-    private void SetState(States state)
+    
+    //int parameter to bypass OnClick() restriction of passing enum and still optimize speed
+    public void SetState(int state)
     {
         switch (state)
         {
-            case States.MENU:
-                
+            case 0:
+                currentState = States.MENU;
                 Time.timeScale = 0;
-                SwitchStates(menustateObjects, gamestateObjects);
                 break;
             
-            case States.GAME:
+            case 1:
+                currentState = States.OPTIONS;
+                Time.timeScale = 0;
+                break;       
+            
+            case 2:
+                currentState = States.GAME;
                 Time.timeScale = 1;
-                SwitchStates(gamestateObjects, menustateObjects);
                 break; 
-
+            
+            case 3:
+                currentState = States.QUIT;
+                print("clicked quit");
+                Application.Quit();
+                break;
+                
         }
     }
-
-    private void SwitchStates(GameObject[] activate, GameObject[] deactivate)
-    {
-        foreach (var obj in deactivate)
-        {
-            obj.SetActive(false);
-        }
-
-        foreach (var obj in activate)
-        {
-            obj.SetActive(true);
-        }
-    }
+    
 
  
 }
